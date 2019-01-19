62a5256b523c12f080e34271debbf04b2959d5af
1964162367@qq.com <1964162367@qq.com>
2019-01-19 23:08:47 +0800

    logs

---
+++ RedAlert/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mainactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"you clicked Button 1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
